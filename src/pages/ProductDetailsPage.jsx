bf43e0f7d0a59101b2a804f5072c24cdc2f74267
giovannimazzi <gioelor@libero.it>
2026-02-20 02:28:12 +0100

    feat: add buttons for prev/next product in ProductDetailsPage.jsx

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { IoIosArrowDropleft } from "react-icons/io";
+import { IoIosArrowDropright } from "react-icons/io";
 
+const productsApiUrl = "https://fakestoreapi.com/products";
 const productApiUrl = "https://fakestoreapi.com/products/:id";
 
 export default function ProductDetailsPage() {
@@ -11,6 +14,14 @@ export default function ProductDetailsPage() {
   const [error, setError] = useState();
   const [loading, setLoading] = useState(true);
 
+  const [nearIds, setNearIds] = useState({ prevId: null, nextId: null });
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getProductData();
+  }, [id]);
+
   const getProductData = () => {
     setLoading(true);
     setError("");
@@ -19,7 +30,11 @@ export default function ProductDetailsPage() {
       .get(productApiUrl.replace(":id", id))
       .then((res) => {
         setProduct(res.data);
-        if (!res.data?.id) navigate("/products");
+        if (!res.data?.id) {
+          navigate("/products");
+        } else {
+          getPrevNextId();
+        }
       })
       .catch((err) => {
         setError(err);
@@ -27,11 +42,31 @@ export default function ProductDetailsPage() {
       .finally(() => setLoading(false));
   };
 
-  useEffect(() => {
-    getProductData();
-  }, [id]);
+  const getPrevNextId = () => {
+    axios
+      .get(productsApiUrl)
+      .then((res) => {
+        const currentId = Number(id);
+        const idsArray = res.data.map((p) => p.id);
+        const idIndex = idsArray.indexOf(currentId);
+        let prevId;
+        let nextId;
+        if (idIndex != -1) {
+          prevId = idIndex === 0 ? null : idsArray[idIndex - 1];
+          nextId =
+            idIndex === idsArray.length - 1 ? null : idsArray[idIndex + 1];
+        } else {
+          prevId = null;
+          nextId = null;
+        }
+        setNearIds({ prevId, nextId });
+      })
+      .catch(setNearIds({ prevId: null, nextId: null }));
+  };
 
-  const navigate = useNavigate();
+  const changeProduct = (newId) => {
+    navigate("/products/" + newId);
+  };
 
   if (loading) {
     return (
@@ -53,8 +88,30 @@ export default function ProductDetailsPage() {
   return (
     <>
       <h2 className="h1 text-primary">DETTAGLI PRODOTTO</h2>
-      <h3 className="mt-5">{product.title}</h3>
-      <img src={product.image} alt={product.title} className="mb-2 w-25" />
+      <div className="d-flex justify-content-between align-items-baseline">
+        <button
+          className="btn border-0"
+          disabled={!nearIds.prevId}
+          onClick={() => changeProduct(nearIds.prevId)}
+        >
+          <IoIosArrowDropleft className="fs-1" />
+        </button>
+        <h3 className="mt-5">{product.title}</h3>
+        <button
+          className="btn border-0"
+          disabled={!nearIds.nextId}
+          onClick={() => changeProduct(nearIds.nextId)}
+        >
+          <IoIosArrowDropright className="fs-1 " />
+        </button>
+      </div>
+      <div className="w-75 mx-auto ratio ratio-16x9">
+        <img
+          src={product.image}
+          alt={product.title}
+          className="object-fit-contain"
+        />
+      </div>
       <h4 className="text-muted">
         {product.category} - € {product.price?.toFixed(2)}
       </h4>
